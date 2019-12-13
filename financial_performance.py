c5dd880ca6e78a055432dcead341e85ce9d13bd6
Jake Dennis <JakeJDennis@gmail.com>
2019-12-12 18:21:33 -0600

    Change to f-strings

--- financial_performance.py
+++ financial_performance.py
@@ -6,38 +6,37 @@ import datetime as dt
 def get_financial_records(ticker):
     def get_fmp_jsondata(url):
         response = urlopen(url)
-        data = response.read().decode("utf-8")
+        data = response.read().decode('utf-8')
         return json.loads(data)
 
-    income_statement_url = "https://financialmodelingprep.com/api/v3/financials/income-statement/" + ticker
-    balance_sheet_url = "https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/" + ticker
-    cash_flow_url = "https://financialmodelingprep.com/api/v3/financials/cash-flow-statement/" + ticker
-    enterprise_values_url = "https://financialmodelingprep.com/api/v3/enterprise-value/" + ticker
-    price_url = "https://financialmodelingprep.com/api/v3/stock/real-time-price/" + ticker
-    
-    try: 
+    income_statement_url = f'https://financialmodelingprep.com/api/v3/financials/income-statement/{ticker}'
+    balance_sheet_url = f'https://financialmodelingprep.com/api/v3/financials/balance-sheet-statement/{ticker}'
+    cash_flow_url = f'https://financialmodelingprep.com/api/v3/financials/cash-flow-statement/{ticker}'
+    enterprise_values_url = f'https://financialmodelingprep.com/api/v3/enterprise-value/{ticker}'
+    price_url = f'https://financialmodelingprep.com/api/v3/stock/real-time-price/{ticker}'
+
+    try:
         income_statement = get_fmp_jsondata(income_statement_url)
         balance_sheet = get_fmp_jsondata(balance_sheet_url)
         statement_of_cash_flows = get_fmp_jsondata(cash_flow_url)
         enterprise_values = get_fmp_jsondata(enterprise_values_url)
         price = get_fmp_jsondata(price_url)
     except:
-        print("Could not query for" + ticker + "on Financial Modeling Prep's API.")
-    
+        print(f"Could not query for {ticker} on Financial Modeling Prep's API.")
+
     try:
         income_statement = pd.DataFrame(income_statement['financials']).transpose()
         balance_sheet = pd.DataFrame(balance_sheet['financials']).transpose()
         statement_of_cash_flows = pd.DataFrame(statement_of_cash_flows['financials']).transpose()
         enterprise_values = pd.DataFrame(enterprise_values['enterpriseValues']).transpose()
     except:
-        print("Unable to process " + ticker + ".")
-    
-    try: 
+        print(f'Unable to process {ticker}.')
+
+    try:
         price = price['price']
-    except: 
-        print("Could not process price for " + ticker + ".")
+    except:
+        print(f'Could not process price for {ticker}.')
         price = float('NaN')
-    
 
     return income_statement, balance_sheet, statement_of_cash_flows, enterprise_values, price
 
--- target_share_prices.py
+++ target_share_prices.py
@@ -1,7 +1,7 @@
 import sys
 import yfinance as yf
-from tabulate import tabulate as tb
 from financial_performance import *
+from tabulate import tabulate as tb
 
 def target_share_prices(ticker):
     print(ticker)
@@ -10,21 +10,21 @@ def target_share_prices(ticker):
     try:
         shares_outstanding = int(float(enterprise_values[0]['Number of Shares']))
     except ValueError:
-        print("---Could not get shares outstanding query for " + ticker + " from Financial Modeling Prep. Trying Yahoo Finance...")
+        print(f'---Could not get shares outstanding query for {ticker} from Financial Modeling Prep. Trying Yahoo Finance...')
         shares_outstanding = yf.Ticker(ticker).info['sharesOutstanding']
     except:
-        print("---Could not get number of outstanding shares from Yahoo Finance...")
+        print('---Could not get number of outstanding shares from Yahoo Finance...')
         shares_outstanding = float('NaN')
-        
+
     def tc_price(income_statement, balance_sheet, statement_of_cash_flows, shares_outstanding):
-        try: 
+        try:
             net_income = float(income_statement[0]['Net Income'])
             net_receivables = float(balance_sheet[0]['Receivables'])-float(balance_sheet[1]['Receivables'])
             net_payables = float(balance_sheet[0]['Payables'])-float(balance_sheet[1]['Payables'])
             income_tax = (float(income_statement[0]['Income Tax Expense']))
             capex = (float(statement_of_cash_flows[0]['Capital Expenditure']))
         except:
-            print("---Unable to calculate Ten Cap price.")
+            print('---Unable to calculate Ten Cap price.')
 
         try:
             ten_cap_price = net_income + net_receivables + net_payables + income_tax + capex
@@ -32,19 +32,19 @@ def target_share_prices(ticker):
         except:
             ten_cap_price = float('NaN')
         return ten_cap_price
-                                                                          
+
 
     def pbt_price(statement_of_cash_flows, shares_outstanding):
         try:
             pbt_price = round(float(statement_of_cash_flows[0]['Free Cash Flow'])*(1.16*8)/float(enterprise_values[0]['Number of Shares']),2)
         except:
-            print("---Unable to calculate Payback Time price.")
+            print('---Unable to calculate Payback Time price.')
             pbt_price = float('NaN')
         return pbt_price
 
     ten_cap_price = tc_price(income_statement, balance_sheet, statement_of_cash_flows, shares_outstanding)
     payback_time_price = pbt_price(statement_of_cash_flows, shares_outstanding)
-    
+
     return price, ten_cap_price, payback_time_price
 
 tickers = open("watchlist.txt", "r")
